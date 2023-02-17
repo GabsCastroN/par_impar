ee11345a6030eb55e6585353aa3d8ed8ef6a1b63
GabsCastroN <gabrielcastro@colegioguanenta.edu.co>
2023-02-17 07:48:37 -0500

    Se creo el programa py, se agrego el readme y el diagrama

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # par_inpar
 Programa para diferenciar par y inpares 
+
+
+![Diagrama de flujo](diagrama.png "diagrama de flujo")
---
+++ par_impar.py
@@ -0,0 +1,11 @@
+#input
+X = int(input("Dame un numero: "))
+
+#Proccesing 
+if X % 2 == 0: 
+    msj="par"
+else: 
+    msj="impar"
+#output 
+
+print("El numero es " + msj)
